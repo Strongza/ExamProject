c25f4b3f3abfcf3da74596e0534bebb3efe3e88c
Monika Porncharoensukkul <monika.porncharoensukkul@scb.co.th>
2019-07-04 08:27:55 +0700

    pass data

--- Project/Controllers/View/PrepareViewController.swift
+++ Project/Controllers/View/PrepareViewController.swift
@@ -4,9 +4,15 @@ import UIKit
 class PrepareViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var deskLabel: UILabel!
+    var id : Int = 0
+    var examName : String = ""
+    var totalScore: Int = 0
+    var examCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.nameLabel.text = "\(examName)"
+        self.deskLabel.text = "ข้อสอบชุดนี้ประกอบด้วย\n -ข้อสอบจำนวน \(totalScore) ข้อ\n -คะแนนรวมทั้งหมด \(examCount) คะแนน"
 
     }
     
