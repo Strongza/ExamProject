06f2de51bb9c0a66183508ab07b832c084435fae
pattarawadee.singhakul <pattarawadee.singhakul>
2019-07-11 08:53:07 +0700

    fix struc to map json from api

---
+++ Project/Models/GoHome.swift
@@ -0,0 +1,68 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let goHome = try? newJSONDecoder().decode(GoHome.self, from: jsonData)
+
+import Foundation
+
+// MARK: - GoHome
+
+    struct GoHome: Codable {
+        let status: StatusGoHome
+        let data: DataClass
+    }
+    
+    // MARK: - DataClass
+    struct DataClass: Codable {
+        let examID: Int
+        let examName: String
+        let questions: [QuestionElement]
+        
+        
+        enum CodingKeys: String, CodingKey {
+            case examID = "exam_id"
+            case examName = "exam_name"
+            case questions
+        }
+    }
+    
+    // MARK: - QuestionElement
+    struct QuestionElement: Codable {
+        let question: QuestionQuestion
+        let choices: [Choice]
+    }
+    
+    // MARK: - Choice
+    struct Choice: Codable {
+        let choiceText: String?
+        let choicePic: String?
+        let choiceScore: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case choiceText = "choice_text"
+            case choicePic = "choice_pic"
+            case choiceScore = "choice_score"
+        }
+    }
+    
+    // MARK: - QuestionQuestion
+    struct QuestionQuestion: Codable {
+        let questionText: String
+        let questionPic: String?
+        let questionType: String
+        
+        enum CodingKeys: String, CodingKey {
+            case questionText = "question_text"
+            case questionPic = "question_pic"
+            case questionType = "question_type"
+        }
+    }
+    
+    // MARK: - Status
+    struct StatusGoHome: Codable {
+        let code: Int
+        let message: String
+    }
+    
+
+
